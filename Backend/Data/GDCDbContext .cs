aaf85ee6c864bd8684f2bf22e6fa93f27c0bc22d
benex drake <benex@hotmail.de>
2025-02-07 22:13:20 +0100

    Change name of DB Context

---
+++ Backend/Data/GDCDbContext .cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Data;
+
+public class GDCDbContext(DbContextOptions<GDCDbContext> options) : DbContext(options)
+{
+    public DbSet<User> User { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Request> Requests { get; set; }
+}
--- Backend/Data/UserDbContext .cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Backend.Data;
-
-public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
-{
-    public DbSet<User> User { get; set; }
-}
