1aefa350ca60a52e068681600fea88a22382ddf5
benex drake <benex@hotmail.de>
2025-02-07 22:14:24 +0100

    adding User for Cross Table

--- Backend/Models/Project.cs
+++ Backend/Models/Project.cs
@@ -7,5 +7,6 @@
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Elements { get; set; } = string.Empty;
+        public User[] Users { get; set; } = [];
     }
 }
