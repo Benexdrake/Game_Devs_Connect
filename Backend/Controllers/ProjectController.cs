3fe90328034d136538c0aeecc543814fa9e55b72
benex drake <benex@hotmail.de>
2025-02-07 22:56:46 +0100

    adding controllers

---
+++ Backend/Controllers/ProjectController.cs
@@ -0,0 +1,56 @@
+﻿namespace Backend.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProjectController(GDCDbContext context) : ControllerBase
+{
+    private readonly GDCDbContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult> GetProjectById(string id)
+    {
+        var project = await _context.Projects.FirstOrDefaultAsync(x => x.Equals(id));
+
+        if (project is null) return NotFound();
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddProject(Project project)
+    {
+        var DbProject = await _context.Projects.FirstOrDefaultAsync(x => x.Id.Equals(project.Id));
+
+        if (DbProject is not null) return BadRequest();
+
+        await _context.Projects.AddAsync(project);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateProject(Project project)
+    {
+        var DbProject = await _context.Projects.FirstOrDefaultAsync(x => x.Id.Equals(project.Id));
+
+        if (DbProject is null) return BadRequest();
+        
+        // Update
+
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteProject(string id)
+    {
+        var project = await _context.Projects.FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+        if (project is null) return NotFound();
+
+        _context.Projects.Remove(project);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ Backend/Controllers/RequestController.cs
@@ -0,0 +1,60 @@
+﻿namespace Backend.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class RequestController(GDCDbContext context) : ControllerBase
+{
+    private readonly GDCDbContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult> GetRequests()
+    {
+        var requests = await _context.Requests.ToListAsync();
+
+        return Ok(requests);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetRequestByID(string id)
+    {
+        var request = await _context.Requests.FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+        return Ok(request);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddRequest(Request request)
+    {
+        var DbRequest = await _context.Requests.FirstOrDefaultAsync(x => x.Id.Equals(request.Id));
+
+        if (DbRequest is not null) return BadRequest();
+
+        await _context.Requests.AddAsync(request);
+        await _context.SaveChangesAsync();
+
+        return Ok(request);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateRequest(Request request)
+    {
+        var Dbrequest = await _context.Requests.FirstOrDefaultAsync(x => x.Id.Equals(request));
+        
+        // Update
+
+        return Ok(request);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteRequest(string id)
+    {
+        var request = await _context.Requests.FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+        if (request is null) return BadRequest();
+
+        _context.Requests.Remove(request);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
