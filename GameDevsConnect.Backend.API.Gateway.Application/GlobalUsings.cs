a99bea08eb80b4acd18cdcf5d8a47994d936aa2e
benex <benex@hotmail.de>
2025-06-30 12:01:11 +0200

    feat(gateway): add model for yarp configuration

--- GameDevsConnect.Backend.API.Gateway.Application/GlobalUsings.cs
+++ GameDevsConnect.Backend.API.Gateway.Application/GlobalUsings.cs
@@ -1,5 +1,1 @@
-﻿global using GameDevsConnect.Backend.API.Gateway.Contract.Responses;
-global using GameDevsConnect.Backend.API.Configuration.Data;
-global using GameDevsConnect.Backend.API.Configuration.DTOs;
-global using Microsoft.EntityFrameworkCore;
-global using Serilog;
+﻿
---
+++ GameDevsConnect.Backend.API.Gateway.Application/Models/APIEndpoint.cs
@@ -0,0 +1,7 @@
+﻿namespace GameDevsConnect.Backend.API.Gateway.Application.Models;
+
+public class APIEndpoint(string name, string url)
+{
+    public string? Name { get; set; } = name;
+    public string? Url { get; set; } = url;
+}
