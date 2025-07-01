631e2bcddb17a57c13dc57f4eed5bbb7b63fbea4
benex <benex@hotmail.de>
2025-07-01 20:52:40 +0200

    feat(api): adding get follower and following counts

--- GameDevsConnect.Backend.API.Profile.Contract/Responses/GetFullResponse.cs
+++ GameDevsConnect.Backend.API.Profile.Contract/Responses/GetFullResponse.cs
@@ -1,7 +1,9 @@
 ﻿namespace GameDevsConnect.Backend.API.Profile.Contract.Responses;
 
-public class GetFullResponse(string message, bool status, UserDTO user, ProfileDTO profile) : ApiResponse(message, status)
+public class GetFullResponse(string message, bool status, UserDTO user, ProfileDTO profile, int followerCount, int followingCount) : ApiResponse(message, status)
 {
     public UserDTO User { get; set; } = user;
     public ProfileDTO Profile { get; set; } = profile;
+    public int FollowerCount { get; set; } = followerCount;
+    public int FollowingCount { get; set; } = followingCount;
 }
