79727a8165e3f80fd811dd49161f86749c85aa4d
benex <benex@hotmail.de>
2025-07-03 13:45:52 +0200

    refactor(api): change Request to Post

--- GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
+++ GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
@@ -5,7 +5,7 @@ namespace GameDevsConnect.Backend.API.Post.Contract.Response;
 
 public class GetFullResponse(string message, bool status, PostDTO? post, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file) : ApiResponse(message, status)
 {
-    public PostDTO? Request { get; set; } = post;
+    public PostDTO? Post { get; set; } = post;
     public TagDTO[]? Tags { get; set; } = tags;
     public string ProjectTitle { get; set; } = projectTitle;
     public UserDTO? Owner { get; set; } = owner;
