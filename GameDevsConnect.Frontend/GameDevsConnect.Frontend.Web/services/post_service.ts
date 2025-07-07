fa18024b0dd53dc7b3c61cdd56aeb293cfbfc675
benex <benex@hotmail.de>
2025-07-07 20:47:51 +0200

    feat(front): add service

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/services/post_service.ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/services/post_service.ts
@@ -1,4 +1,9 @@
+import { IUpsertPostRequest } from "@/interfaces/requests/post/api_add_post_request"
+import { IAPIResponse } from "@/interfaces/responses/api_response"
+import { IAPIPostIdsResponse } from "@/interfaces/responses/post/api_post_ids_response"
+import { IAPIPostResponse } from "@/interfaces/responses/post/api_post_response"
 import { url } from "@/lib/api"
+import axios from "axios"
 
 
 
@@ -6,3 +11,28 @@ const getUrl = url('get','post')
 const postUrl = url('post','post')
 const putUrl = url('put','post')
 const deleteUrl = url('delete','post')
+
+export const getPostIdsAsync = async (page:number=1, pageSize:number=10, searchTerm:string='') =>
+{
+    return await axios.get<IAPIPostIdsResponse>(`${getUrl}?page=${page}&pageSize=${pageSize}&searchTerm=${searchTerm}`).then(x => x.data)
+}
+
+export const getPostAsync = async (id:string) =>
+{
+    return await axios.get<IAPIPostResponse>(`${getUrl}/full/${id}`).then(x => x.data)
+}
+
+export const addPostAsync = async (addPost:IUpsertPostRequest) =>
+{
+    return await axios.post<IAPIResponse>(`${postUrl}/add`, addPost).then(x => x.data)
+}
+
+export const updatePost = async (updatePost:IUpsertPostRequest) =>
+{
+    return await axios.put<IAPIResponse>(`${putUrl}/update`, updatePost).then(x => x.data)
+}
+
+export const deletePost = async (id:string) =>
+{
+    return await axios.delete<IAPIResponse>(`${deleteUrl}/delete/${id}`)
+}
