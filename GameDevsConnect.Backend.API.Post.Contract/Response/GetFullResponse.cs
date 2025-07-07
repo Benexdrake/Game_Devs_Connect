f7b99a2ff26b08fe1a511f8f2ead6384425c6d41
benex <benex@hotmail.de>
2025-07-07 20:43:40 +0200

    feat(response): add more props

--- GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
+++ GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
@@ -1,13 +1,13 @@
-﻿using GameDevsConnect.Backend.API.Configuration.Application.DTOs;
-using GameDevsConnect.Backend.API.Configuration.Contract.Responses;
+﻿namespace GameDevsConnect.Backend.API.Post.Contract.Response;
 
-namespace GameDevsConnect.Backend.API.Post.Contract.Response;
-
-public class GetFullResponse(string message, bool status, PostDTO? post, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file) : ApiResponse(message, status)
+public class GetFullResponse(string message, bool status, PostDTO? post, string[] questIdss, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file, int comments, int likes) : ApiResponse(message, status)
 {
     public PostDTO? Post { get; set; } = post;
+    public string[] QuestIdss { get; set; } = questIdss;
     public TagDTO[]? Tags { get; set; } = tags;
     public string ProjectTitle { get; set; } = projectTitle;
     public UserDTO? Owner { get; set; } = owner;
     public FileDTO? File { get; set; } = file;
+    public int Comments { get; set; } = comments;
+    public int Likes { get; set; } = likes;
 }
