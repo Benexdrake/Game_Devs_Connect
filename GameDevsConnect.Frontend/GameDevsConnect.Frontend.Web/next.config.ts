a1271739e4ea9e1f96ed92b73c077b53d4d25092
benex <benex@hotmail.de>
2025-07-08 14:05:55 +0200

    refactor(front): change config because of error

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/next.config.ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = 
+{
+  productionBrowserSourceMaps: false,
+  webpack(config, { dev, isServer }) {
+    if (dev && !isServer) {
+      config.devtool = false;
+    }
+    return config;
+  },
   reactStrictMode: true,
 };
 
