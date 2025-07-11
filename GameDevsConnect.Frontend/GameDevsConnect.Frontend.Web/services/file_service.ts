e3edec528058286466e433707df214f9e2d9c3d9
benex <benex@hotmail.de>
2025-07-11 12:25:08 +0200

    feat: add services

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/services/file_service.ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/services/file_service.ts
@@ -1,4 +1,8 @@
+import { IFile } from "@/interfaces/file"
+import { IAPIFileAddResponse } from "@/interfaces/responses/file/api_file_add_response"
+import { IAPIFileGetResponse } from "@/interfaces/responses/file/api_file_get_response"
 import { url } from "@/lib/api"
+import axios from "axios"
 
 
 
@@ -7,3 +11,15 @@ const getUrl = url('get','file')
 const postUrl = url('post','file')
 const putUrl = url('put','file')
 const deleteUrl = url('delete','file')
+
+
+
+export const getFileByIdAsync = async (fileId:string) =>
+{
+    return await axios.get<IAPIFileGetResponse>(`${getUrl}/${fileId}`).then(x => x.data);
+}
+
+export const addFileAsync = async (file:IFile) =>
+{
+    return await axios.post<IAPIFileAddResponse>(`${postUrl}/add`, file).then(x => x.data);
+}
