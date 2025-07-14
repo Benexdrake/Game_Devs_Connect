c72001053ce3ea6ab20f01aaadde9fd4a74d663a
benex <benex@hotmail.de>
2025-07-14 17:40:26 +0200

    feat: update on add post and adding a comment to a post

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/components/post/add_post.tsx
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/components/post/add_post.tsx
@@ -2,61 +2,51 @@ import styles from '@/styles/post/add_post.module.css'
 import { useRouter } from 'next/router';
 import { IPost } from "@/interfaces/post";
 import { ITag } from "@/interfaces/tag";
-import { useEffect, useState } from "react";
-
-import { getTags } from "@/services/tag_service";
+import { useState } from "react";
 import SelectTags from "../tag/select_tags";
 import { IUpsertPostRequest } from '@/interfaces/requests/post/api_add_post_request';
 import { addPostAsync } from '@/services/post_service';
-import { addFileAsync, getFileByIdAsync } from '@/services/file_service';
+import { addFileAsync } from '@/services/file_service';
 import { IFile } from '@/interfaces/file';
 
-export default function AddPost(props:any)
-{
-    const {userId} = props;
-    const [tags, setTags] = useState<ITag[]>([]);
+export default function AddPost(props: any) {
+    const { userId, postId, page } = props;
     const [selectedTags, setSelectedTags] = useState<ITag[]>([])
-
+    const [showtags, setShowTags] = useState<boolean>(false)
     const [message, setMessage] = useState<string>('');
+
+    const [addFile, setAddFile] = useState<boolean>(false);
 
     const router = useRouter();
 
-    useEffect(() => {
-        const get = async () =>
-        {
-            const response = await getTags();
-            if(response.status)
-                setTags(response.tags);
-        }
-        get();
-    }, [])
 
     // Quests - Falls Quest Post
-    
+
     // File
 
     // Choose Project or none
     // Need Project Names with IDs
-    
-    const sendHandler = async () =>
+
+    const sendHandler = async () => 
     {
         // Upload File and get Url
 
         // Add File
-        const file:IFile = {id:'', url:'https://fantasyanime.com/finalfantasy/ff/maps/Final-Fantasy-1-GBA-World-Map-Labeled.png', size:1000, type:'image', ownerId:userId, created:null}
-        const fileResponse = await addFileAsync(file)
+        let fileId = '';
+        if (addFile) {
+            const file: IFile = { id: '', url: 'https://fantasyanime.com/finalfantasy/ff/maps/Final-Fantasy-1-GBA-World-Map-Labeled.png', size: 1000, type: 'image', ownerId: userId, created: null }
+            const fileResponse = await addFileAsync(file)
 
-        console.log(fileResponse);
+            if (fileResponse.status)
+                fileId = fileResponse.id;
+        }
 
-        const getFileResponse = await getFileByIdAsync(fileResponse.id);
-        console.log(getFileResponse);
-        
-        
 
-        const post:IPost = { id:'', parentId:'', hasQuest:false, message, created:null, projectId:'', ownerId: userId, fileId:fileResponse.id, isDeleted:false, completed:false}
-        const addPost:IUpsertPostRequest = {post, tags:selectedTags}
+
+        const post: IPost = { id: '', parentId: postId, hasQuest: false, message, created: null, projectId: '', ownerId: userId, fileId, isDeleted: false, completed: false }
+        const addPost: IUpsertPostRequest = { post, tags: selectedTags }
         console.log(addPost);
-        
+
         // Validation
         // Message min x Zeichen
         // Tags min 1 Tag
@@ -64,11 +54,13 @@ export default function AddPost(props:any)
         //
 
         const response = await addPostAsync(addPost)
-        console.log(response);
-        if(router.pathname === '/')
+
+        if(postId)
+            router.reload();
+        else if (router.pathname === '/')
             router.reload();
         else
-            router.push('/')   
+            router.push('/')
     }
 
     return (
@@ -77,7 +69,7 @@ export default function AddPost(props:any)
                 <textarea name="message" id="message" onChange={(text) => setMessage(text.target.value)}></textarea>
             </div>
             <div className={styles.buttons}>
-                <div className={styles.button}>
+                <div className={styles.button} onClick={() => setAddFile(!addFile)}>
                     <i className="fa-regular fa-image"></i>
                 </div>
                 <div className={styles.button}>
@@ -95,19 +87,42 @@ export default function AddPost(props:any)
                 </div>
             </div>
             <div className={styles.show_files}>
-                <div className={styles.show_file}>
-                    <div className={styles.show_file__type}><i className="fa-regular fa-image"></i></div>
-                    <div className={styles.show_file__name}><p>gamedevsconnect.png</p></div>
-                    <div className={styles.show_file__delete}><i className="fa-solid fa-xmark"></i></div>
-                </div>
+                {addFile && (
+                    <div className={styles.show_file}>
+                        <div className={styles.show_file__type}><i className="fa-regular fa-image"></i></div>
+                        <div className={styles.show_file__name}><p>gamedevsconnect.png</p></div>
+                        <div className={styles.show_file__delete}><i className="fa-solid fa-xmark"></i></div>
+                    </div>
+                )}
             </div>
+            {page && (
+                <>
+                    <div onClick={() => setShowTags(!showtags)}>
+                        {!showtags ?
+                            (
+                                <div className={styles.show_tags_button}>
+                                    <p>Show Tags <i className="fa-solid fa-square-caret-down"></i></p>
+                                </div>
+                            )
+                            :
+                            (
+                                <div className={styles.show_tags_button}>
+                                    <p>Close Tags <i className="fa-solid fa-square-minus"></i></p>
+                                </div>
+                            )
+                        }
+                    </div>
 
-            <div className={styles.tags}>
-                <SelectTags tags={tags} setSelectedTags={setSelectedTags} selectedTags={selectedTags}/>
-            </div>
+                    <div className={styles.tags}>
+                        {showtags && (
+                            <SelectTags setSelectedTags={setSelectedTags} selectedTags={selectedTags} />
+                        )}
+                    </div>
+                </>
+            )}
             <div className={styles.footer}>
                 <div className={styles.send_button} onClick={sendHandler}>
-                <p>Send</p>
+                    <p>Send</p>
                 </div>
             </div>
         </div>
