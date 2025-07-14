1b1c8a1231fdb90721604a0e77977ddaff3cde80
benex <benex@hotmail.de>
2025-07-14 17:43:05 +0200

    feat: add a way to get query params

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/api/get/[...path].ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/api/get/[...path].ts
@@ -8,17 +8,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(400).send('Access Denied!');
         return;
     }
-    let paths = req.query.path as string[];
+    let paths = req.query.path as string[]; 
 
     if(paths.length < 3)
     {
         res.status(400).send('Wrong path');
         return;
     }
+
+    let params = '';
     
     let url = `${process.env.BACKEND_URL}/${paths.join('/')}`
+    
+    const urlSplit = req.url?.split('?') as string[]
+    if(urlSplit?.length === 2)
+        params = `?${urlSplit[1]}`
 
-    let response =  await (await getAxiosInstance()).get(url).then(x => x.data)
+    let response =  await (await getAxiosInstance()).get(`${url}${params}`).then(x => x.data)
 
     res.status(200).json(response);
 }
