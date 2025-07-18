2dabfd9c5f882b50c609ca3dac83ced481de0bb8
benex <benex@hotmail.de>
2025-07-18 18:31:58 +0200

    feat: add response with id

---
+++ GameDevsConnect.Backend.API.Post.Contract/Response/AddResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace GameDevsConnect.Backend.API.Post.Contract.Response;
+
+public class AddResponse(string message, bool status, string id, string[] validationErrors) : ApiResponse(message, status, validationErrors)
+{
+    public string Id { get; set; } = id;
+}
