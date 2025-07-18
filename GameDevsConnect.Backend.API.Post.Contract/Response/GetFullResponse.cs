50c0753b43640ceb9c0ae271fd7c2e879432d40d
benex <benex@hotmail.de>
2025-07-18 18:32:28 +0200

    refactor: change quest ids to number/count

--- GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
+++ GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
@@ -1,9 +1,9 @@
 ﻿namespace GameDevsConnect.Backend.API.Post.Contract.Response;
 
-public class GetFullResponse(string message, bool status, PostDTO? post, string[] questIdss, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file, int comments, int likes) : ApiResponse(message, status)
+public class GetFullResponse(string message, bool status, PostDTO? post, int questCount, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file, int comments, int likes) : ApiResponse(message, status)
 {
     public PostDTO? Post { get; set; } = post;
-    public string[] QuestIdss { get; set; } = questIdss;
+    public int QuestCount { get; set; } = questCount;
     public TagDTO[]? Tags { get; set; } = tags;
     public string ProjectTitle { get; set; } = projectTitle;
     public UserDTO? Owner { get; set; } = owner;
