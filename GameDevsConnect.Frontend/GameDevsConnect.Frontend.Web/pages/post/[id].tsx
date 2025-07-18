e609b47f606e5988d84a1f818d13e3b1647ea9f8
benex <benex@hotmail.de>
2025-07-18 18:38:56 +0200

    feat: add quests, files and comments with element blocks

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/post/[id].tsx
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/post/[id].tsx
@@ -4,34 +4,47 @@ import { getSession, useSession } from "next-auth/react";
 import AddPost from "@/components/post/add_post";
 import { IUser } from "@/interfaces/user";
 import { getPostIdsAsync } from "@/services/post_service";
+import { getQuestIdsByPostIdAsync } from "@/services/quest_service";
+import ShowQuest from "@/components/quest/show_quest";
+import ShowElement from "@/components/show_element";
 
 export default function Post(props:any)
 {
-    const {id, commentIds} = props;
+    const {id, commentIds, questIds, fileIds} = props;
 
     const {data:session} = useSession();
     const user = session?.user as IUser;
 
-    console.log(commentIds);
-    
-
     if(session)
     return (
-        <div>
-            {id && (
+        <div style={{display:"grid", gap:'8px', margin:'16px 0'}}>
+            { id && (
                 <div>
                     <ShowPost id={id} page={true}/>
                 </div>
             )}
-            <div>
-                FILES
-            </div>
-            <div>
-                <AddPost userId={user.id} postId={id} page={true}/>
-            </div>
-            <div>
-                {commentIds && commentIds.map((x:string) => <ShowPost id={x} page={false}/>)}
-            </div>
+            { questIds && questIds.length > 0 && 
+                (
+                    <ShowElement title='Quests'>{ 
+                        <div style={{display:"grid", gap:'8px', marginTop:'8px'}}>
+                            {questIds.map((x:string) => <ShowQuest id={x} key={x}/>)} 
+                        </div>
+                    }</ShowElement>
+                )
+            }
+            { fileIds && fileIds.length > 0 && 
+                (
+                    <ShowElement title='Files'>{ fileIds.map((x:string) => <>Show Files</>) }</ShowElement>
+                )
+            }
+            <AddPost userId={user.id} postId={id} page={true}/>
+            {commentIds && commentIds.length > 0 && (
+                <ShowElement title='Comments'>
+                    <div style={{display:"grid", gap:'8px', marginTop:'8px'}}>
+                        {commentIds.map((x:string) => <ShowPost id={x} page={false} key={x}/>)}
+                    </div> 
+                </ShowElement>
+            )}
         </div>
     )
 }
@@ -60,14 +73,14 @@ export async function getServerSideProps(context:GetServerSidePropsContext)
         }
     
 
-    const response = await getPostIdsAsync(1,10,'',id as string);
-    console.log(response.ids);
-
+    const responseComments = await getPostIdsAsync(1,10,'',id as string);
+    const responseQuestIds = await getQuestIdsByPostIdAsync(id as string);
     
     return {
         props: {
             id,
-            commentIds:response.status ? response.ids : ''
+            commentIds:responseComments.status ? responseComments.ids : [],
+            questIds: responseQuestIds.status ? responseQuestIds.ids : []
         }
     }
 }
