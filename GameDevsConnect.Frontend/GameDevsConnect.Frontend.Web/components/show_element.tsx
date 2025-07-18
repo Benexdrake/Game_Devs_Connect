566689c3572bc710fd399b86faec3087a903f583
benex <benex@hotmail.de>
2025-07-18 18:33:11 +0200

    feat: add button element for showing elements

---
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/components/show_element.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import styles from '@/styles/showElement.module.css'
+
+export default function ShowElement(props:any)
+{
+    const {title, children} = props
+    const [show, setShow] = useState<boolean>(false);
+    const [load, setLoad] = useState<boolean>(false);
+
+        const showHandler = () =>
+    {
+        setShow(!show)
+        if(!load)
+            setLoad(true);
+    }
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.button}onClick={() => showHandler()}>
+                <p>{show? 'Close':'Open'} {title} <i className={show? 'fa-solid fa-square-minus':'fa-solid fa-square-caret-down'}></i></p>
+            </div>
+            <div style={{visibility:show?'visible':'hidden', height:show?'fit-content':'0px'}}>
+                {load && (
+                    <div>
+                        {children}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
