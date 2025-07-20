03dd5afd52c4c0ff698befba84251e8c14b9b5a3
benex <benex@hotmail.de>
2025-07-20 11:39:32 +0200

    feat: add favorite request response

---
+++ GameDevsConnect.Backend.API.Quest.Contract/Requests/FavoriteQuestResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace GameDevsConnect.Backend.API.Quest.Contract.Requests;
+
+public class FavoriteQuestResponse
+{
+    public string QuestId { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
