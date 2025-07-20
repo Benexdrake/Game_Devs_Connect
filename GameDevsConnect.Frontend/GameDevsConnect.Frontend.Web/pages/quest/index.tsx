c4773bd9a77b5a48b4922f0d38ba27ef74332fd9
benex <benex@hotmail.de>
2025-07-20 20:48:49 +0200

    feat: add list quests faved by user

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/quest/index.tsx
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/pages/quest/index.tsx
@@ -1,9 +1,43 @@
-export default function Quests(props:any)
+import InfiniteScrollQuests from "@/components/quest/infinite_scroll_quests";
+import { IUser } from "@/interfaces/user";
+import { getFavoritedQuestIdsAsync } from "@/services/quest_service";
+import { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
+
+export default function Quests({ids, userId}: {ids:string[], userId:string})
 {
 
     return (
         <div>
-            See all Favorited Quests from logged in User
+            <InfiniteScrollQuests initialIds={ids} userId={userId} search=""/>
         </div>
     )
+}
+
+export async function getServerSideProps(context:GetServerSidePropsContext)
+{
+      const session = await getSession(context)
+      
+      if(!session)
+      {
+          return {
+              redirect: {
+                  destination: '/login',
+                  permanent: false
+              }
+          }
+      }
+
+      const user = session.user as IUser;
+
+      const response = await getFavoritedQuestIdsAsync(user.id)
+      console.log(response);
+      
+
+      return {
+        props: {
+            ids: response.ids,
+            userId:user.id
+        }
+      }
 }
