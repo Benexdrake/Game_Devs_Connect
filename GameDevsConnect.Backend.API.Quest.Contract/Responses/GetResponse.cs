df4fc8a1b8638d746b4e16dd17fb4ac1c1c4a37a
benex <benex@hotmail.de>
2025-07-20 11:40:07 +0200

    feat: add favorited quest for get response

--- GameDevsConnect.Backend.API.Quest.Contract/Responses/GetResponse.cs
+++ GameDevsConnect.Backend.API.Quest.Contract/Responses/GetResponse.cs
@@ -1,7 +1,6 @@
-﻿using GameDevsConnect.Backend.API.Configuration.Contract.Responses;
-
-namespace GameDevsConnect.Backend.API.Quest.Contract.Responses;
-public class GetResponse(string message, bool status, QuestDTO quest) : ApiResponse(message, status)
+﻿namespace GameDevsConnect.Backend.API.Quest.Contract.Responses;
+public class GetResponse(string message, bool status, QuestDTO quest, bool favoritedQuest) : ApiResponse(message, status)
 {
     public QuestDTO Quest { get; set; } = quest;
+    public bool FavoritedQuest { get; set; } = favoritedQuest;
 }
