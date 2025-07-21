434006950d9c04be6e246fc4aa771262a2058e24
benex <benex@hotmail.de>
2025-07-21 14:01:08 +0200

    refactor: change name to blob request

--- GameDevsConnect.Backend.API.Azure.Contract/Requests/BlobRequest.cs
+++ GameDevsConnect.Backend.API.Azure.Contract/Requests/BlobRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameDevsConnect.Backend.API.Azure.Contract.Requests;
 
-public class UploadRequest
+public class BlobRequest
 {
     public string FileName { get; set; }
     public string ContainerName { get; set; }
