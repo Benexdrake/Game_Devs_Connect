486bf5d28de806cee245f29bf599499952d4d1a0
benex <benex@hotmail.de>
2025-07-21 14:04:23 +0200

    feat: add response type for id

---
+++ GameDevsConnect.Backend.API.User.Contract/Responses/GetUserIdResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace GameDevsConnect.Backend.API.User.Contract.Responses;
+
+public class GetUserIdResponse(string message, bool status, string userId, string[] validateErrors) : ApiResponse(message, status, validateErrors)
+{
+    public string Id { get; set; } = userId;
+}
