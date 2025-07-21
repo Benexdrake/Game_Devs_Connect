ee54e5cd0ce2927a024731919777cc32dcc8b7b8
benex <benex@hotmail.de>
2025-07-21 13:59:57 +0200

    feat: add upload request

---
+++ GameDevsConnect.Backend.API.Azure.Contract/Requests/UploadRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace GameDevsConnect.Backend.API.Azure.Contract.Requests;
+
+public class UploadRequest
+{
+    public string FileName { get; set; }
+    public string ContainerName { get; set; }
+}
