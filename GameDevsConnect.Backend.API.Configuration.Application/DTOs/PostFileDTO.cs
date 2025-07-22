fc1e69ba0a01429acdc881152186417f22f4b394
benex <benex@hotmail.de>
2025-07-22 13:24:08 +0200

    feat: add post file dto

---
+++ GameDevsConnect.Backend.API.Configuration.Application/DTOs/PostFileDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace GameDevsConnect.Backend.API.Configuration.Application.DTOs;
+
+public class PostFileDTO
+{
+    public string PostId { get; set; }
+    public string FileId { get; set; }
+}
