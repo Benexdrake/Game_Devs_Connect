cb2ec587972212890131419d5cab3a7fe7bd89a9
benex <benex@hotmail.de>
2025-07-22 13:29:44 +0200

    refactor: change switch to if for image

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/components/post/post_content.tsx
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/components/post/post_content.tsx
@@ -3,25 +3,10 @@ import styles from '@/styles/post/post.module.css'
 
 export default function PostContent({file}: {file:IFile})
 {
-    switch(file.type)
-    {
-        case 'image':
-            return (
+    if(file.type.includes("image"))
+        return (
                 <div>
                     <img src={file.url} alt={file.type} className={styles.file}/>
                 </div>
-            )
-        case 'video':
-            return (
-                <div>
-                    YOUTUBE
-                </div>
-            )
-        default:
-            return (
-                <div>
-
-                </div>
-            )
-    }
+        )
 }
