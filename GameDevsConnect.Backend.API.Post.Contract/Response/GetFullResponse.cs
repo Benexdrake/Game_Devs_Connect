01320133ed6af4a36432617208936ea8ddc35d03
benex <benex@hotmail.de>
2025-07-22 13:27:29 +0200

    refactor: change single filedto to array

--- GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
+++ GameDevsConnect.Backend.API.Post.Contract/Response/GetFullResponse.cs
@@ -1,13 +1,13 @@
 ﻿namespace GameDevsConnect.Backend.API.Post.Contract.Response;
 
-public class GetFullResponse(string message, bool status, PostDTO? post, int questCount, TagDTO[]? tags, string projectTitle, UserDTO? owner, FileDTO? file, int comments, int likes) : ApiResponse(message, status)
+public class GetFullResponse(string message, bool status, PostDTO? post, FileDTO[] files, int questCount, TagDTO[]? tags, string projectTitle, UserDTO? owner, int comments, int likes) : ApiResponse(message, status)
 {
     public PostDTO? Post { get; set; } = post;
+    public FileDTO[] Files { get; set; } = files;
     public int QuestCount { get; set; } = questCount;
     public TagDTO[]? Tags { get; set; } = tags;
     public string ProjectTitle { get; set; } = projectTitle;
     public UserDTO? Owner { get; set; } = owner;
-    public FileDTO? File { get; set; } = file;
     public int Comments { get; set; } = comments;
     public int Likes { get; set; } = likes;
 }
