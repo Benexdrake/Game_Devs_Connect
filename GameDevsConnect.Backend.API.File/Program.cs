e8eb11641908296422621081e3cc05c4428f94cc
benex <benex@hotmail.de>
2025-07-22 13:24:33 +0200

    feat: add env variable

--- GameDevsConnect.Backend.API.File/Program.cs
+++ GameDevsConnect.Backend.API.File/Program.cs
@@ -3,6 +3,8 @@ var builder = start.Build(args);
 
 builder.Services.AddScoped<IFileRepository, FileRepository>();
 
+builder.Configuration["AZURE_STORAGE_BASE_URL"] = Environment.GetEnvironmentVariable("AZURE_STORAGE_BASE_URL");
+
 var app = start.Create(builder);
 
 app.MapEndpointsV1();
