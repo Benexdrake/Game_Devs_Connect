f926595f0a0324ee924e94288a36f303d0bb7790
benex <benex@hotmail.de>
2025-07-22 13:26:59 +0200

    feat: add file ids for request

--- GameDevsConnect.Backend.API.Post.Contract/Request/UpsertPost.cs
+++ GameDevsConnect.Backend.API.Post.Contract/Request/UpsertPost.cs
@@ -1,7 +1,8 @@
 ﻿namespace GameDevsConnect.Backend.API.Post.Contract.Request;
 
-public class UpsertPost(PostDTO? post, TagDTO[]? tags)
+public class UpsertPost(PostDTO? post, TagDTO[]? tags, string[] fileIds)
 {
     public PostDTO? Post { get; set; } = post;
     public TagDTO[]? Tags { get; set; } = tags;
+    public string[] FileIds { get; set; } = fileIds;
 }
