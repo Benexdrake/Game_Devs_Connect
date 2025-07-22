16a76e21b785c40b296de914deeac4e3234a2137
benex <benex@hotmail.de>
2025-07-22 13:31:21 +0200

    refactor: change single file to array of files

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/interfaces/responses/post/api_post_response.ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/interfaces/responses/post/api_post_response.ts
@@ -1,17 +1,17 @@
 import { ITag } from "@/interfaces/tag";
 import { IAPIResponse } from "../api_response";
 import { IUser } from "@/interfaces/user";
-import { IFile } from "@/interfaces/file";
 import { IPost } from "@/interfaces/post";
+import { IFile } from "@/interfaces/file";
 
 export interface IAPIPostResponse extends IAPIResponse
 {
     post:IPost;
+    files:IFile[];
     questCount:number;
     tags: ITag[] | null;
     projectTitle:string;
     owner:IUser;
-    file:IFile | null;
     comments:number;
     likes:number;
 }
