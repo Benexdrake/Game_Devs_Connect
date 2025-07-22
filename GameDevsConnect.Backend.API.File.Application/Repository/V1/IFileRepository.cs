d256a0a3141706e2ad182c19a4fae3722e84b163
benex <benex@hotmail.de>
2025-07-22 13:26:01 +0200

    refactor: delete get by post parent id

--- GameDevsConnect.Backend.API.File.Application/Repository/V1/IFileRepository.cs
+++ GameDevsConnect.Backend.API.File.Application/Repository/V1/IFileRepository.cs
@@ -6,5 +6,4 @@ public interface IFileRepository
     Task<AddResponse> AddAsync(FileDTO file, CancellationToken token);
     Task<ApiResponse> UpdateAsync(FileDTO file, CancellationToken token);
     Task<ApiResponse> DeleteAsync(string fileId, CancellationToken token);
-    Task<GetIdsbyId> GetByPostParentIdAsync(string parentId, CancellationToken token);
 }
