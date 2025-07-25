56ab9fb2be30a5aeac81a1ebcd34990adb79f59c
benex <benex@hotmail.de>
2025-07-25 18:41:58 +0200

    refactor: update

--- GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/lib/api.ts
+++ GameDevsConnect.Frontend/GameDevsConnect.Frontend.Web/lib/api.ts
@@ -2,7 +2,8 @@ import { PUBLIC_API_URL } from "./constant";
 
 export const getUrl = (methode:string, endpoint:string) => 
 {
-    console.log("NEXTJS BACKEND URL",`${PUBLIC_API_URL}/api/${methode}/api/v1/${endpoint}`);
+    console.log("NEXTJS BACKEND URL1",`${PUBLIC_API_URL}/api/${methode}/api/v1/${endpoint}`);
+    console.log("NEXTJS BACKEND URL2",`${process.env.NEXT_PUBLIC_URL}`);
     
     return `${PUBLIC_API_URL}/api/${methode}/api/v1/${endpoint}`;
 };
