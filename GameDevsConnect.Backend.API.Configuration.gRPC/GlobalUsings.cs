6ed874a8ace8d9d352cf9eac1d8f28fcd14039bd
benex <benex@hotmail.de>
2025-08-09 10:05:04 +0200

    deleted

--- GameDevsConnect.Backend.API.Configuration.gRPC/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-﻿global using GameDevsConnect.Backend.API.Configuration.Application.Data;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.OpenApi.Models;
--- GameDevsConnect.Backend.API.User.gRPC/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using GameDevsConnect.Backend.API.User.Application.Repository.V1;
-global using GameDevsConnect.Backend.API.User.gRPC.Services;
-global using GameDevsConnect.Backend.API.Configuration.Application.DTOs;
-global using GameDevsConnect.Backend.API.Configuration.Application.Data;
-global using GameDevsConnect.Backend.API.User.Application.Validators;
-global using Google.Protobuf.WellKnownTypes;
-global using Grpc.Core;
-global using GameDevsConnect.Backend.API.Configuration.gRPC;
