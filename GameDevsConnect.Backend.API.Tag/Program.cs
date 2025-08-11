2249a514408a6b1727341380b8c15349a290e173
benex <benex@hotmail.de>
2025-08-11 10:12:21 +0200

    feat: add modus for http and or grcp

--- GameDevsConnect.Backend.API.Tag/Program.cs
+++ GameDevsConnect.Backend.API.Tag/Program.cs
@@ -5,5 +5,16 @@ builder.Services.AddScoped<ITagRepository, TagRepository>();
 
 var app = start.Create(builder);
 
-app.MapEndpointsV1();
+string modus = "0";
+
+if (args.Length > 0)
+    modus = args[0];
+
+// HTTP1
+if (modus.Equals("0") || modus.Equals("1"))
+    app.MapEndpointsV1();
+
+// HTTP2
+if (modus.Equals("0") || modus.Equals("2"))
+    app.MapGrpcService<APIService>();
 app.Run();
