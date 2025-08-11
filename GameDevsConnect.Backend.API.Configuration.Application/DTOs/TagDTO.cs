7e45175348044327af6187e4da6a1ec149538825
benex <benex@hotmail.de>
2025-08-11 10:10:11 +0200

    feat: add constructors

--- GameDevsConnect.Backend.API.Configuration.Application/DTOs/TagDTO.cs
+++ GameDevsConnect.Backend.API.Configuration.Application/DTOs/TagDTO.cs
@@ -4,4 +4,12 @@ public partial class TagDTO
 {
     public string Tag { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
+
+    public TagDTO() {}
+
+    public TagDTO(string tag, string type)
+    {
+        Tag = tag;
+        Type = type;
+    }
 }
