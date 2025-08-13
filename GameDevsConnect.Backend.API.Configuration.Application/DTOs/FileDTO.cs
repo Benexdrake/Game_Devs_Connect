04aca1203af21cbf1e1d864e83defc5107a3b3d2
benex <benex@hotmail.de>
2025-08-13 16:42:41 +0200

    refactor: delete null type thing ?

--- GameDevsConnect.Backend.API.Configuration.Application/DTOs/FileDTO.cs
+++ GameDevsConnect.Backend.API.Configuration.Application/DTOs/FileDTO.cs
@@ -2,16 +2,16 @@
 
 public partial class FileDTO
 {
-    public string? Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
-    public string? Url { get; set; }
+    public string Url { get; set; } = string.Empty;
 
-    public string? Type { get; set; }
-    public string? Extension { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public string Extension { get; set; } = string.Empty;
 
-    public int? Size { get; set; }
+    public int Size { get; set; }
 
     public DateTime? Created { get; set; }
 
-    public string? OwnerId { get; set; }
+    public string OwnerId { get; set; } = string.Empty;
 }
